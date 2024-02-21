190f43e0c51236c2ee09286837a5cb1b41a6ba7a
Tejas PANDYA <160587142+iamtejaspandya@users.noreply.github.com>
2024-02-21 15:13:58 +0530

    Add files via upload

---
+++ AddPlugins/Plugin/Cart.php
@@ -0,0 +1,17 @@
+<?php
+ 
+namespace Tejas\AddPlugins\Plugin;
+ 
+class Cart
+{
+    public function beforeAddProduct(
+       \Magento\Checkout\Model\Cart $subject,
+       $productInfo,
+       $requestInfo = null
+    )
+    {
+       $productInfo['price'] += 15;
+       $requestInfo['qty'] = 30;
+       return array($productInfo, $requestInfo);
+    }
+}
